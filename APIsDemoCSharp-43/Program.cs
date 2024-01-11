20a9ec2c92fc10377fda8821bfb5fe25fa1f106c
Seth Bowman <81124768+SethBowman@users.noreply.github.com>
2024-01-11 13:21:23 -0600

    Update Program.cs

--- APIsDemoCSharp-43/Program.cs
+++ APIsDemoCSharp-43/Program.cs
@@ -8,7 +8,7 @@ var kanyeURL = "https://api.kanye.rest/";
 
 //Using the HTTPClient instance we made above
 //Send a GET request to the url created above, this is going to give us back a string of json
-var kanyeResponse =client.GetStringAsync(kanyeURL).Result;
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
 
 //Parse the JSON response we just got back into JOject, we do this so we can access certain pieces of the JSON
 //In this case we will be getting the value of "quote" which we be will actual quote itself and the whole JSON body
@@ -23,7 +23,7 @@ Console.WriteLine(kanyeQuote);
 //Grab appsettings file
 var appsettingsText = File.ReadAllText("appsettings.json");
 
-//Get the api key from the appseetings file using it's name "apiKey"
+//Get the api key from the appsettings file using it's name "apiKey"
 var apiKey = JObject.Parse(appsettingsText).GetValue("apiKey").ToString();
 
 //Build the api url using the provided params you chose (I chose the zip code option) along with the api key
